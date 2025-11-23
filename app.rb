95dc72903ae6fb0cbe35974bbe48c7929106d4f9
Tevildo <team@bsofia.net>
2025-11-23 03:15:52 +0200

    Add disclaimer

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'uri'
 require 'pathname'
 
-ARCHIVE_PATH = Pathname.new(ARGV[0] || '')
+ARCHIVE_PATH = Pathname.new(ARGV[0] || '').freeze
 ROUTING_TABLE = {}
 
 puts 'Loading archive manifest, and populating routing table...'
@@ -16,12 +16,17 @@ JSON.load_file(ARCHIVE_PATH.join('manifest.json')).each do |f|
   ROUTING_TABLE[uri.path][uri.query.to_s] = f.merge('host' => uri.host)
 end
 
+ROUTING_TABLE.freeze
+
+DISCLAIMER = File.read(Pathname.new(__dir__).join('disclaimer.html')).freeze
+
 get '/' do
   route = ROUTING_TABLE['/'][request.query_string]
 
   halt 404, 'not found' unless route
 
-  send_file Pathname.new('.').join(ARCHIVE_PATH, route['host'], route['file_id'], 'index.html')
+  File.read(Pathname.new('.').join(ARCHIVE_PATH, route['host'], route['file_id'], 'index.html'))
+    .gsub('<div id="map', "#{DISCLAIMER}<div id=\"map")
 end
 
 get '/health' do
